9e7ef8378ae075b6ada4a7881e0ade71185eeb74
abhiram rayala <sunny.rayala@msitprogram.net>
2019-11-02 21:40:16 +0530

    abc

--- day 2/assignment 3/Parentheses Matching/Solution.java
+++ day 2/assignment 3/Parentheses Matching/Solution.java
@@ -1,20 +1,20 @@
-import java.util.*;
-class Solution{
+import java.util.Stack;
+class Solution {
 
-	public static String isMatching(String str){
-		Stack <Character> stack = new Stack<Character>() ;
-		for (int i=0;i<str.length();i++) {
-			if (i==0 && (str.charAt(i)==']' || str.charAt(i)=='}' || str.charAt(i)==')')){
+	public static String isMatching(final String str){
+		Stack <Character> stack = new Stack<Character>();
+		for (int i = 0; i < str.length(); i++) {
+			if (i == 0 && (str.charAt(i) == ']' || str.charAt(i) == '}' || str.charAt(i) == ')')){
 				return "NO";
 			}
-			if (str.charAt(i)=='[' || str.charAt(i)=='{' || str.charAt(i)=='(') {
+			if (str.charAt(i) == '[' || str.charAt(i) == '{' || str.charAt(i) == '(') {
 				stack.push(str.charAt(i));
 			}
-			else if ((str.charAt(i)==']' || str.charAt(i)=='}' || str.charAt(i)==')') && !stack.isEmpty()) {
+			else if ((str.charAt(i) == ']' || str.charAt(i) == '}' || str.charAt(i) == ')') && !stack.isEmpty()) {
 				stack.pop();
 			}
 		}
-		if (stack.size()!=0) {
+		if (stack.size() != 0) {
 			return "NO";
 		}
 		return "YES";
