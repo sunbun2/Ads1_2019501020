df80309edb4552e2557e8095d454c18f816fcb35
abhiram rayala <sunny.rayala@msitprogram.net>
2019-11-03 18:48:31 +0530

    abc

--- day 2/assign 4/Solution.java
+++ day 2/assign 4/Solution.java
@@ -1,9 +1,22 @@
 import java.util.Queue;
 import java.util.LinkedList;
+/**
+ * This class is used to implement the game.
+ * @author Abhiram Rayala
+ */
+
 class Solution{
+	private Solution() {
+	}
+	/**
+	 * This method returns a string in the order of dequeue of the elements.
+	 * @param a the number of inputs starting from 0.
+	 * @param b the value at which the dequeue should be done
+	 * @return string yes or no.
+	 */
 	public static String Josephus(final int a, final int b){
 	String s = "";
-	Queue<Integer> queue = new Queue();
+	Queue<Integer> queue = new LinkedList<>();
 	for (int t = 0; t < a; t++) {
 	    queue.add(t);
 	}
