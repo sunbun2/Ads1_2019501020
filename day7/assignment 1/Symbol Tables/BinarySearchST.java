e4066a4c9d5b870e810de39aad5bf1e082cb06aa
abhiram rayala <sunny.rayala@msitprogram.net>
2019-11-08 16:46:20 +0530

    abc

--- day7/assignment 1/Symbol Tables/BinarySearchST.java
+++ day7/assignment 1/Symbol Tables/BinarySearchST.java
@@ -1,26 +1,117 @@
+import java.util.Queue;
+
 public class BinarySearchST<Key extends Comparable<Key>, Value> {
-    int[] keys;
-    String[] value;
-    int point;
-    BinarySearchST(){
-        keys = new Keys[1000];
-        value = new Value[1000];
-        point=0;
+    private Key[] keys;
+    private Value[] vals;
+    private int n = 0;
+
+    public BinarySearchST() { 
+        keys = (Key[]) new Comparable[1000]; 
+        vals = (Value[]) new Object[1000]; 
+    }   
+
+    public int  rank(Key k) {
+        int lo = 0, hi = n-1; 
+        while (lo <= hi) { 
+            int mid = lo + (hi - lo) / 2; 
+            int cmp = k.compareTo(keys[mid]);
+            if      (cmp < 0) hi = mid - 1; 
+            else if (cmp > 0) lo = mid + 1; 
+            else return mid; 
+        } 
+        return lo;                                                                                                                                                                                                                                                                                                                        
     }
     public void put(Key k, Value v) {
-        keys[point] = k;
-        value[point] = v;
-    }
-    Value get(Key k) {
-        if (keys == null) throw new IllegalArgumentException("calls get() with a null key");
-        key = Keys[0];
-        int cmp = k.compareTo(key);
-        if      (cmp < 0) return get(x.left, key);
-        else if (cmp > 0) return get(x.right, key);
-        else              return x.val;
-    }
-    boolean contains(Key k) {
+        if (v == null) {
+            delete(k);
+            return;
+        }
 
+        int i = rank(k);
+
+        // key is already in table
+        if (i < n && keys[i].compareTo(k) == 0) {
+            vals[i] = v;
+            return;
+        }
+
+
+        for (int j = n; j > i; j--)  {
+            keys[j] = keys[j-1];
+            vals[j] = vals[j-1];
+        }
+        keys[i] = k;
+        vals[i] = v;
+        n++;
+    }
+
+    public Value get(Key k) {
+        int i = rank(k);
+        if (i < n && k.compareTo(keys[i]) == 0) return vals[i];
+        else return null;
+    }
+
+    boolean contains(Key k) {
+        return get(k) != null;
+    }
+
+    Key max() {
+        return keys[n-1];
+    }
+
+    Key floor(Key k) {
+        int i = rank(k);
+        if (i < n && k.compareTo(keys[i]) == 0) return keys[i];
+        if (i == 0) return null;
+        else return keys[i-1];
+    }
+
+    public void delete(Key key) {
+        if (n == 0) return;
+
+        // compute rank
+        int i = rank(key);
+
+        // key not in table
+        if (i == n || keys[i].compareTo(key) != 0) {
+            return;
+        }
+
+        for (int j = i; j < n-1; j++)  {
+            keys[j] = keys[j+1];
+            vals[j] = vals[j+1];
+        }
+
+        n--;
+        keys[n] = null;  // to avoid loitering
+        vals[n] = null;
+
+    } 
+
+    public void deleteMin() {
+        delete(min());
+    }
+
+    Key min() {
+        return keys[0];
+    }
+
+    public Key[] keys() {
+        if (isEmpty()) {
+            return null;
+        }
+        return keys;
+    }
+    public boolean isEmpty() {
+        return n == 0;
+    }
+
+    public String toString() {
+        String str = "";
+        for (int i = 0; i < n; i++) {
+            str += keys[i] + " ";
+        }
+        return str;
     }
 
 }
