2cc4e244df6f578e3a8dca8db96c8773b2977629
abhiram rayala <sunny.rayala@msitprogram.net>
2019-11-10 18:04:11 +0530

    abc

--- day 8/assignment 1/BST/BST.java
+++ day 8/assignment 1/BST/BST.java
@@ -596,6 +596,22 @@ Value extends Comparable<Value>> {
 		queue.enqueue(node.key);
 		inorder(node.right, queue);
 	}
+	
+	private void keys(Node x, Queue<Key> queue, Key lo, Key hi) { 
+        if (x == null) return; 
+        int cmplo = lo.compareTo(x.key); 
+        int cmphi = hi.compareTo(x.key); 
+        if (cmplo < 0) keys(x.left, queue, lo, hi); 
+        if (cmplo <= 0 && cmphi >= 0) queue.enqueue(x.key); 
+        if (cmphi > 0) keys(x.right, queue, lo, hi); 
+    } 
+
+
+	public Iterable<Key> keys(Key lo, Key hi) {
+        Queue<Key> queue = new Queue<Key>();
+        keys(root, queue, lo, hi);
+        return queue;
+    } 
 
 	// public static void main(String[] args) {
 	// 	BST<Integer, String> bst = new BST<Integer, String>();
